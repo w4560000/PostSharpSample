16082116c52632ec664ec39d64ea98b5c5e6c411
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-10-16 18:55:56 +0800

    add Logging.Audit (無法記錄到 物件參數)

---
+++ Logging/PostSharpSample.Logging.Audit/PurchaseOrder.cs
@@ -0,0 +1,30 @@
+﻿using PostSharp.Patterns.Diagnostics.Audit;
+
+namespace PostSharpSample.Logging.Audit
+{
+    public class PurchaseOrder : BusinessObject
+    {
+        // Not the Audit aspect on this method.
+        [Audit]
+        public void Approve(string comment = null)
+        {
+            // Details skipped.
+            var a = 1;
+        }
+
+        // Not the Audit aspect on this method.
+        [Audit]
+        public string Approve(Test qqq)
+        {
+            return "123";
+        }
+    }
+
+    public class Test
+    {
+        public int QQQ
+        {
+            get; set;
+        }
+    }
+}
