27505ff09453c7797458bbf4e20d7bd1ac03d784
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-11-01 18:52:13 +0800

    add Common WPF MVVM

---
+++ INotifyPropertyChanged/PostSharpSample.WPF/Posts.cs
@@ -0,0 +1,9 @@
+﻿namespace PostSharpSample.WPF
+{
+    public class Posts
+    {
+        public string postsText { get; set; }
+        public string postsTitle1 { get; set; }
+        public string postsTitle2 { get; set; }
+    }
+}
---
+++ INotifyPropertyChanged/PostSharpSample.WPF/PostsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace PostSharpSample.WPF
+{
+    public class PostsViewModel : INotifyPropertyChanged
+    {
+        public Posts posts { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PostsViewModel()
+        {
+            posts = new Posts { postsText = "", postsTitle1 = "postsTitle1Unknown", postsTitle2 = "postsTitle2Unknown" };
+        }
+
+        public string PostsTitle1
+        {
+            get { return posts.postsTitle1; }
+            set
+            {
+                if (posts.postsTitle1 != value)
+                {
+                    posts.postsTitle1 = value;
+                    RaisePropertyChanged(nameof(Posts.postsTitle1));
+                }
+            }
+        }
+
+        public string PostsTitle2
+        {
+            get { return posts.postsTitle2; }
+            set
+            {
+                if (posts.postsTitle2 != value)
+                {
+                    posts.postsTitle2 = value;
+                    RaisePropertyChanged(nameof(Posts.postsTitle2));
+                }
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
