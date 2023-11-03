0a80aaa2be83eb1da8a436aaf469ddb3641bb326
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-11-03 19:08:37 +0800

    add WeekEvent Sample

---
+++ WeakEvent/PostSharpSample.WeakEvent/DelegateReferenceKeeper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace PostSharpSample.WeakEvent
+{
+    internal static class DelegateReferenceKeeper
+    {
+        private static readonly object staticObj = new object();
+
+        private static readonly ConditionalWeakTable<object, List<Delegate>> table =
+          new ConditionalWeakTable<object, List<Delegate>>();
+
+        public static void AddReference(Delegate handler)
+        {
+            var list = table.GetOrCreateValue(handler.Target ?? staticObj);
+            lock (list)
+            {
+                list.Add(handler);
+            }
+        }
+
+        public static void RemoveReference(Delegate handler)
+        {
+            List<Delegate> list;
+            if (table.TryGetValue(handler.Target ?? staticObj, out list))
+            {
+                lock (list)
+                {
+                    list.Remove(handler);
+                }
+            }
+        }
+    }
+}
---
+++ WeakEvent/PostSharpSample.WeakEvent/Program.cs
@@ -0,0 +1,60 @@
+﻿using PostSharp.Aspects.Advices;
+using PostSharp.Patterns.Model;
+using System;
+
+namespace PostSharpSample.WeakEvent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // The logic of this sample needs to be separated into methods,
+            // because .NET Framework CLR doesn't collect objects within a scope of a method.
+
+            var weakReference = AddCollectedEventClient();
+
+            GC.Collect();
+
+            Console.WriteLine("Client is alive: {0} (should be False)", weakReference.IsAlive);
+
+            // Raise the event when the client is dead.
+            EventClient.EventHandlerCount = 0;
+            MyEvent.Invoke(null, EventArgs.Empty);
+            Console.WriteLine("EventHandlerCount: {0} (should be 0)", EventClient.EventHandlerCount);
+
+            Console.ReadKey();
+        }
+
+        private static WeakReference AddCollectedEventClient()
+        {
+            var eventClient = new EventClient();
+            MyEvent += eventClient.EventHandler;
+
+            // Forcing GC here to prove that we are not collecting the handler when the client is alive.
+            GC.Collect();
+
+            // Raise the event when the client is alive.
+            MyEvent?.Invoke(null, EventArgs.Empty);
+            Console.WriteLine("EventHandlerCount: {0} (should be 1)", EventClient.EventHandlerCount);
+
+            //MyEvent -= eventClient.EventHandler;
+
+            // Cause the client to be collected.
+            return new WeakReference(eventClient);
+        }
+
+        [WeakEvent]
+        private static event EventHandler MyEvent;
+    }
+
+    internal class EventClient
+    {
+        public static int EventHandlerCount;
+
+        public void EventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Oops!");
+            EventHandlerCount++;
+        }
+    }
+}
